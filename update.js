cc9ddf2c6c961cf93c3e18a30a6988636377e9b6
Thibault <thibaultchatillon@gmail.com>
2023-10-05 15:39:55 +0200

    5min test

--- update.js
+++ update.js
@@ -1,1 +1,27 @@
-console.log('update !!!');
+require('dotenv').config()
+const axios = require('axios');
+const fs = require('fs');
+
+
+
+axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`)
+    .then(response => format(response.data))
+    .catch(error => {
+        console.log(error);
+        // read backup.md and write to README.md
+        const str = fs.readFileSync('backup.md');
+        fs.writeFileSync('README.md', str)
+
+    });
+
+function format(data) {
+    const txt = `# ${data.title}
+<img src="${data.url}" alt="nasa picture of the day" width="800"/>
+
+${data.explanation}
+readme updated at ${new Date().toLocaleString()}
+`
+
+    fs.writeFileSync('README.md', txt)
+
+}
